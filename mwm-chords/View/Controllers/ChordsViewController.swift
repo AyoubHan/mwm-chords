7e55ee1e8d0545fcfb7e45f1fe8740d3cd26c23c
papuruu <papuruu@papuruus-MacBook-Pro.local>
2021-12-22 22:11:28 +0100

    data label functional

--- mwm-chords/View/Controllers/ChordsViewController.swift
+++ mwm-chords/View/Controllers/ChordsViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 class ChordsViewController: UIViewController {
     
     // let chordViewModel = ChordsViewModel()
+    var toneNames: [String] = []
+    var keyChords: [String] = []
     let chordService = ChordsService()
-    let pickerData = PickerData()
+//    let pickerData = PickerData()
     
     @IBOutlet weak var chordTonesPickerView: UIPickerView!
+    @IBOutlet weak var chordsDataLabel: UILabel!
     
     // MARK: - Life Cycle
     
@@ -24,15 +27,15 @@ class ChordsViewController: UIViewController {
     }
     
     func setIt() {
-        chordTonesPickerView.delegate = pickerData
-        chordTonesPickerView.dataSource = pickerData
+        chordTonesPickerView.delegate = self
+        chordTonesPickerView.dataSource = self
     }
     
     private func loadChordTones() {
         chordService.getChordTones { (result) in
             switch result {
             case .success(let namess):
-                self.pickerData.toneNames = namess
+                self.toneNames = namess
                 self.loadKeyChords()
             case .failure(let error):
                 print(error)
@@ -44,11 +47,46 @@ class ChordsViewController: UIViewController {
         chordService.getKeyChords { (result) in
             switch result {
             case .success(let suffix):
-                self.pickerData.keyChords = suffix
+                self.keyChords = suffix
             case .failure(let error):
                 print(error)
             }
             self.chordTonesPickerView.reloadAllComponents()
         }
+    }
+}
+
+extension ChordsViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        var count = 0
+        if component == 0 {
+            count = toneNames.count
+        } else {
+            count = keyChords.count
+        }
+        return count
+    }
+}
+
+extension ChordsViewController: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        var rowData = ""
+        if component == 0 {
+            rowData = toneNames[row]
+        } else {
+            rowData = keyChords[row]
+        }
+        return rowData
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let tone = toneNames[pickerView.selectedRow(inComponent: 0)]
+        let key = keyChords[pickerView.selectedRow(inComponent: 1)]
+        chordsDataLabel.text = "Tone: \(tone) \nKey: \(key)"
     }
 }
--- mwm-chords/View/UIPickers/PickerData.swift
+++ mwm-chords/View/UIPickers/PickerData.swift
@@ -8,41 +8,41 @@
 import Foundation
 import UIKit
 
-class PickerData: NSObject {
-    var toneNames: [String] = []
-    var keyChords: [String] = []
-}
-
-extension PickerData: UIPickerViewDataSource {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 2
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        var count = 0
-        if component == 0 {
-            count = toneNames.count
-        } else {
-            count = keyChords.count
-        }
-        return count
-    }
-}
-
-extension PickerData: UIPickerViewDelegate {
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        var rowData = ""
-        if component == 0 {
-            rowData = toneNames[row]
-        } else {
-            rowData = keyChords[row]
-        }
-        return rowData
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        print(toneNames[row])
-        print(keyChords[row])
-    }
-}
+//class PickerData: NSObject {
+//    var toneNames: [String] = []
+//    var keyChords: [String] = []
+//    var dataLabel = ""
+//}
+//
+//extension PickerData: UIPickerViewDataSource {
+//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//        return 2
+//    }
+//    
+//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//        var count = 0
+//        if component == 0 {
+//            count = toneNames.count
+//        } else {
+//            count = keyChords.count
+//        }
+//        return count
+//    }
+//}
+//
+//extension PickerData: UIPickerViewDelegate {
+//    
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        var rowData = ""
+//        if component == 0 {
+//            rowData = toneNames[row]
+//        } else {
+//            rowData = keyChords[row]
+//        }
+//        return rowData
+//    }
+//    
+//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        dataLabel = "\(toneNames[row]) h lkd"
+//    }
+//}
